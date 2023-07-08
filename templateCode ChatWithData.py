cd5a00c67b0fb22182478a1b18de63fef588920f
tomwojcik0 <51424286+tomwojcik0@users.noreply.github.com>
2023-07-07 17:06:43 -0700

    code from the course

--- templateCode ChatWithData.py
+++ templateCode ChatWithData.py
@@ -15,8 +15,521 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
+from langchain.document_loaders import PyPDFLoader
+
+# Load PDF
+loaders = [
+    # Duplicate documents on purpose - messy data
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf"),  # dup on purpose
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture02.pdf"),
+    PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture03.pdf")
+]
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
+
+# Split
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 150
+)
+
+splits = text_splitter.split_documents(docs)
+
+len(splits)
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+embedding = OpenAIEmbeddings()
+
+# example to check embedding correlations...
+sentence1 = "i like dogs"
+sentence2 = "i like canines"
+sentence3 = "the weather is ugly outside"
+
+embedding1 = embedding.embed_query(sentence1)
+embedding2 = embedding.embed_query(sentence2)
+embedding3 = embedding.embed_query(sentence3)
+
+import numpy as np
+np.dot(embedding1, embedding2)
+np.dot(embedding1, embedding3)
+np.dot(embedding2, embedding3)
+
+  # Vectorstores...using Chromadb, because in-memory, easy to work with
+# ! pip install chromadb  
+from langchain.vectorstores import Chroma
+persist_directory = 'docs/chroma/'
+!rm -rf ./docs/chroma  # remove old database files if any
+
+vectordb = Chroma.from_documents(
+    documents=splits,
+    embedding=embedding,
+    persist_directory=persist_directory
+)
+
+print(vectordb._collection.count())
+
+  # Similarity search...
+question = "is there an email i can ask for help"
+docs = vectordb.similarity_search(question,k=3)
+len(docs)
+docs[0].page_content
+
+  # saving vector db
+vectordb.persist()
+
+question = "what did they say about matlab?"
+docs = vectordb.similarity_search(question,k=5)
+
+  # notice docs 0 and 1 identical...
+docs[0]
+docs[1]
+
+  # example query...
+question = "what did they say about regression in the third lecture?"
+docs = vectordb.similarity_search(question,k=5)
+
+for doc in docs:
+    print(doc.metadata)
+
+print(docs[4].page_content)
 
 # RETRIEVAL
+# goal: retrieve the most relevant splits
+# there are several retrieval techniques..
+
+#!pip install lark
+
+# similarity search:
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+persist_directory = 'docs/chroma/'
+
+print(vectordb._collection.count())
+
+texts = [
+    """The Amanita phalloides has a large and imposing epigeous (aboveground) fruiting body (basidiocarp).""",
+    """A mushroom with a large fruiting body is the Amanita phalloides. Some varieties are all-white.""",
+    """A. phalloides, a.k.a Death Cap, is one of the most poisonous of all known mushrooms.""",
+
+smalldb = Chroma.from_texts(texts, embedding=embedding)
+
+question = "Tell me about all-white mushrooms with large fruiting bodies"
+
+smalldb.similarity_search(question, k=2)
+
+smalldb.max_marginal_relevance_search(question,k=2, fetch_k=3)
+
+# addressing diversity: maximum marginal relevance (MMR)
+question = "what did they say about matlab?"
+docs_ss = vectordb.similarity_search(question,k=3)
+
+docs_ss[0].page_content[:100]
+docs_ss[1].page_content[:100]
+
+docs_mmr = vectordb.max_marginal_relevance_search(question,k=3)
+
+docs_mmr[0].page_content[:100]
+docs_mmr[1].page_content[:100]
+
+# Addressing Specificity: adding metadata filters
+question = "what did they say about regression in the third lecture?"
+docs = vectordb.similarity_search(
+    question,
+    k=3,
+    filter={"source":"docs/cs229_lectures/MachineLearning-Lecture03.pdf"}
+)
+
+for d in docs:
+    print(d.metadata)
+
+  # self query retriever
+from langchain.llms import OpenAI
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+
+metadata_field_info = [
+    AttributeInfo(
+        name="source",
+        description="The lecture the chunk is from, should be one of `docs/cs229_lectures/MachineLearning-Lecture01.pdf`, `docs/cs229_lectures/MachineLearning-Lecture02.pdf`, or `docs/cs229_lectures/MachineLearning-Lecture03.pdf`",
+        type="string",
+    ),
+    AttributeInfo(
+        name="page",
+        description="The page from the lecture",
+        type="integer",
+    ),
+]
+
+document_content_description = "Lecture notes"
+llm = OpenAI(temperature=0)
+retriever = SelfQueryRetriever.from_llm(
+    llm,
+    vectordb,
+    document_content_description,
+    metadata_field_info,
+    verbose=True
+)
+
+question = "what did they say about regression in the third lecture?"
+
+docs = retriever.get_relevant_documents(question)
+
+for d in docs:
+    print(d.metadata)
+
+  # Compression: a ways to sub-select only the most relevant info from splits (?)
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+
+def pretty_print_docs(docs):
+    print(f"\n{'-' * 100}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]))
+
+  # Wrap our vectorstore
+llm = OpenAI(temperature=0)
+compressor = LLMChainExtractor.from_llm(llm)
+
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor,
+    base_retriever=vectordb.as_retriever()
+)
+
+question = "what did they say about matlab?"
+compressed_docs = compression_retriever.get_relevant_documents(question)
+pretty_print_docs(compressed_docs)
+
+  # Combo of techniques...
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor,
+    base_retriever=vectordb.as_retriever(search_type = "mmr")
+)
+question = "what did they say about matlab?"
+compressed_docs = compression_retriever.get_relevant_documents(question)
+pretty_print_docs(compressed_docs)
+
+  # other ways to do retrieval (besides vectordb): SVM, TF-IDF
+from langchain.retrievers import SVMRetriever
+from langchain.retrievers import TFIDFRetriever
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Load PDF
+loader = PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf")
+pages = loader.load()
+all_page_text=[p.page_content for p in pages]
+joined_page_text=" ".join(all_page_text)
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1500,chunk_overlap = 150)
+splits = text_splitter.split_text(joined_page_text)
+
+# Retrieve
+svm_retriever = SVMRetriever.from_texts(splits,embedding)
+tfidf_retriever = TFIDFRetriever.from_texts(splits)
+
+question = "What are major topics for this class?"
+docs_svm=svm_retriever.get_relevant_documents(question)
+docs_svm[0]
+
+question = "what did they say about matlab?"
+docs_tfidf=tfidf_retriever.get_relevant_documents(question)
+docs_tfidf[0]
+
+
 # QUESTION ANSWERING
+import datetime
+current_date = datetime.datetime.now().date()
+if current_date < datetime.date(2023, 9, 2):
+    llm_name = "gpt-3.5-turbo-0301"
+else:
+    llm_name = "gpt-3.5-turbo"
+print(llm_name)
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+persist_directory = 'docs/chroma/'
+embedding = OpenAIEmbeddings()
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+
+print(vectordb._collection.count())
+
+question = "What are major topics for this class?"
+docs = vectordb.similarity_search(question,k=3)
+len(docs)
+
+from langchain.chat_models import ChatOpenAI
+llm = ChatOpenAI(model_name=llm_name, temperature=0)
+
+  # RetrievalQA chain...
+from langchain.chains import RetrievalQA
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever()
+)
+
+result = qa_chain({"query": question})
+result["result"]
+
+    # Build prompt
+from langchain.prompts import PromptTemplate
+
+template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+# Run chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+
+question = "Is probability a class topic?"
+
+result = qa_chain({"query": question})
+result["result"]
+result["source_documents"][0]
+
+qa_chain_mr = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    chain_type="map_reduce"
+)
+
+result = qa_chain_mr({"query": question})
+result["result"]
+
+# To experiment with langchain platform...
+#import os
+#os.environ["LANGCHAIN_TRACING_V2"] = "true"
+#os.environ["LANGCHAIN_ENDPOINT"] = "https://api.langchain.plus"
+#os.environ["LANGCHAIN_API_KEY"] = "..." # replace dots with your api key
+
+qa_chain_mr = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    chain_type="map_reduce"
+)
+result = qa_chain_mr({"query": question})
+result["result"]
+
+qa_chain_mr = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    chain_type="refine"
+)
+result = qa_chain_mr({"query": question})
+result["result"]
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever()
+)
+
+question = "Is probability a class topic?"
+result = qa_chain({"query": question})
+result["result"]
+
+question = "why are those prerequesites needed?"
+result = qa_chain({"query": question})
+result["result"]
+
+
 # CHAT
+  # assigning openai llm version (repeat code)
+import datetime
+current_date = datetime.datetime.now().date()
+if current_date < datetime.date(2023, 9, 2):
+    llm_name = "gpt-3.5-turbo-0301"
+else:
+    llm_name = "gpt-3.5-turbo"
+print(llm_name)
+
+  # working with memory buffer for convos
+from langchain.memory import ConversationBufferMemory
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
+
+from langchain.chains import ConversationalRetrievalChain
+retriever=vectordb.as_retriever()
+qa = ConversationalRetrievalChain.from_llm(
+    llm,
+    retriever=retriever,
+    memory=memory
+)
+
+question = "Is probability a class topic?"
+result = qa({"question": question})
+
+result['answer']
+
+question = "why are those prerequesites needed?"
+result = qa({"question": question})
+result['answer']
+
+    # Create a chatbot tha works on your documents...
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.document_loaders import TextLoader
+from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+
+def load_db(file, chain_type, k):
+    # load documents
+    loader = PyPDFLoader(file)
+    documents = loader.load()
+    # split documents
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    docs = text_splitter.split_documents(documents)
+    # define embedding
+    embeddings = OpenAIEmbeddings()
+    # create vector database from data
+    db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+    # define retriever
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
+    # create a chatbot chain. Memory is managed externally.
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model_name=llm_name, temperature=0), 
+        chain_type=chain_type, 
+        retriever=retriever, 
+        return_source_documents=True,
+        return_generated_question=True,
+    )
+    return qa 
+
+      # the UI stuff...(?)
+import panel as pn
+import param
+
+class cbfs(param.Parameterized):
+    chat_history = param.List([])
+    answer = param.String("")
+    db_query  = param.String("")
+    db_response = param.List([])
+    
+    def __init__(self,  **params):
+        super(cbfs, self).__init__( **params)
+        self.panels = []
+        self.loaded_file = "docs/cs229_lectures/MachineLearning-Lecture01.pdf"
+        self.qa = load_db(self.loaded_file,"stuff", 4)
+    
+    def call_load_db(self, count):
+        if count == 0 or file_input.value is None:  # init or no file specified :
+            return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+        else:
+            file_input.save("temp.pdf")  # local copy
+            self.loaded_file = file_input.filename
+            button_load.button_style="outline"
+            self.qa = load_db("temp.pdf", "stuff", 4)
+            button_load.button_style="solid"
+        self.clr_history()
+        return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+
+    def convchain(self, query):
+        if not query:
+            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+        result = self.qa({"question": query, "chat_history": self.chat_history})
+        self.chat_history.extend([(query, result["answer"])])
+        self.db_query = result["generated_question"]
+        self.db_response = result["source_documents"]
+        self.answer = result['answer'] 
+        self.panels.extend([
+            pn.Row('User:', pn.pane.Markdown(query, width=600)),
+            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, width=600, style={'background-color': '#F6F6F6'}))
+        ])
+        inp.value = ''  #clears loading indicator when cleared
+        return pn.WidgetBox(*self.panels,scroll=True)
+
+    @param.depends('db_query ', )
+    def get_lquest(self):
+        if not self.db_query :
+            return pn.Column(
+                pn.Row(pn.pane.Markdown(f"Last question to DB:", styles={'background-color': '#F6F6F6'})),
+                pn.Row(pn.pane.Str("no DB accesses so far"))
+            )
+        return pn.Column(
+            pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),
+            pn.pane.Str(self.db_query )
+        )
+
+    @param.depends('db_response', )
+    def get_sources(self):
+        if not self.db_response:
+            return 
+        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", styles={'background-color': '#F6F6F6'}))]
+        for doc in self.db_response:
+            rlist.append(pn.Row(pn.pane.Str(doc)))
+        return pn.WidgetBox(*rlist, width=600, scroll=True)
+
+    @param.depends('convchain', 'clr_history') 
+    def get_chats(self):
+        if not self.chat_history:
+            return pn.WidgetBox(pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True)
+        rlist=[pn.Row(pn.pane.Markdown(f"Current Chat History variable", styles={'background-color': '#F6F6F6'}))]
+        for exchange in self.chat_history:
+            rlist.append(pn.Row(pn.pane.Str(exchange)))
+        return pn.WidgetBox(*rlist, width=600, scroll=True)
+
+    def clr_history(self,count=0):
+        self.chat_history = []
+        return 
+
+    # create a chatbot
+cb = cbfs()
+
+file_input = pn.widgets.FileInput(accept='.pdf')
+button_load = pn.widgets.Button(name="Load DB", button_type='primary')
+button_clearhistory = pn.widgets.Button(name="Clear History", button_type='warning')
+button_clearhistory.on_click(cb.clr_history)
+inp = pn.widgets.TextInput( placeholder='Enter text here…')
+
+bound_button_load = pn.bind(cb.call_load_db, button_load.param.clicks)
+conversation = pn.bind(cb.convchain, inp) 
+
+jpg_pane = pn.pane.Image( './img/convchain.jpg')
+
+tab1 = pn.Column(
+    pn.Row(inp),
+    pn.layout.Divider(),
+    pn.panel(conversation,  loading_indicator=True, height=300),
+    pn.layout.Divider(),
+)
+tab2= pn.Column(
+    pn.panel(cb.get_lquest),
+    pn.layout.Divider(),
+    pn.panel(cb.get_sources ),
+)
+tab3= pn.Column(
+    pn.panel(cb.get_chats),
+    pn.layout.Divider(),
+)
+tab4=pn.Column(
+    pn.Row( file_input, button_load, bound_button_load),
+    pn.Row( button_clearhistory, pn.pane.Markdown("Clears chat history. Can use to start a new topic" )),
+    pn.layout.Divider(),
+    pn.Row(jpg_pane.clone(width=400))
+)
+dashboard = pn.Column(
+    pn.Row(pn.pane.Markdown('# ChatWithYourData_Bot')),
+    pn.Tabs(('Conversation', tab1), ('Database', tab2), ('Chat History', tab3),('Configure', tab4))
+)
+dashboard
+
+# relevant github by Sophia Yang:  https://github.com/sophiamyang/tutorials-LangChain
+
+
+
 # CONCLUSION
+
+
