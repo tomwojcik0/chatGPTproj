17aff5380f51c333db7bd3e82b963a1c498729e2
tomwojcik0 <51424286+tomwojcik0@users.noreply.github.com>
2023-07-28 11:33:54 -0700

    Update Gradio course.py

--- Gradio course.py
+++ Gradio course.py
@@ -1,1 +1,68 @@
-# 
+# notes and template code from Gradio course on deeplearning.ai
+# gradio is a simple visual front end interface
+# example 1: simple text summarization app with front end
+
+# Load your HF API key and relevant Python libraries.
+import os
+import io
+from IPython.display import Image, display, HTML
+from PIL import Image
+import base64 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+hf_api_key = os.environ['HF_API_KEY']
+
+# Helper function
+import requests, json
+
+#Summarization endpoint
+def get_completion(inputs, parameters=None,ENDPOINT_URL=os.environ['HF_API_SUMMARY_BASE']): 
+    headers = {
+      "Authorization": f"Bearer {hf_api_key}",
+      "Content-Type": "application/json"
+    }
+    data = { "inputs": inputs }
+    if parameters is not None:
+        data.update({"parameters": parameters})
+    response = requests.request("POST",
+                                ENDPOINT_URL, headers=headers,
+                                data=json.dumps(data)
+                               )
+    return json.loads(response.content.decode("utf-8"))
+
+# Building a text summarization app
+# Here we are using an Inference Endpoint for the shleifer/distilbart-cnn-12-6, a 306M parameter distilled model from facebook/bart-large-cnn.
+# How about running it locally?  The code would look very similar if you were running it locally instead of from an API. 
+# The same is true for all the models in the rest of the course, make sure to check the Pipelines documentation page
+
+from transformers import pipeline
+
+get_completion = pipeline("summarization", model="shleifer/distilbart-cnn-12-6")
+
+def summarize(input):
+    output = get_completion(input)
+    return output[0]['summary_text']
+
+# run example in a cell...
+text = ('''The tower is 324 metres (1,063 ft) tall, about the same height
+        as an 81-storey building, and the tallest structure in Paris. 
+        Its base is square, measuring 125 metres (410 ft) on each side. 
+        During its construction, the Eiffel Tower surpassed the Washington 
+        Monument to become the tallest man-made structure in the world,
+        a title it held for 41 years until the Chrysler Building
+        in New York City was finished in 1930. It was the first structure 
+        to reach a height of 300 metres. Due to the addition of a broadcasting 
+        aerial at the top of the tower in 1957, it is now taller than the 
+        Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the 
+        Eiffel Tower is the second tallest free-standing structure in France 
+        after the Millau Viaduct.''')
+
+get_completion(text)
+
+
+
+
+
+
+# example 2: app for named entity recognition model (NER)
+
